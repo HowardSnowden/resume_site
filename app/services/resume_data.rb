0e1508a61b7af9af465ed2f66785fecf508c214b
Damien Pyles <damien.pyles@readymaderc.com>
2017-11-12 12:14:52 -0500

    change job limit

--- app/services/resume_data.rb
+++ app/services/resume_data.rb
@@ -3,7 +3,7 @@ class ResumeData
   attr_accessor :jobs, :summary, :education, :projects, :skill_categories, :other_jobs, :references
   def initialize(**params)
 
-  	@jobs = Job.where(type: 'Job').limit(5).order(display_order: :asc).includes(:bullet_points)
+  	@jobs = Job.where(type: 'Job').limit(6).order(display_order: :asc).includes(:bullet_points)
   	@summary = Summary.first || Summary.new
   	@education = Education.limit(5).order(created_at: :desc)
   	@projects = Project.limit(10).order(display_order: :asc).includes(:bullet_points)
