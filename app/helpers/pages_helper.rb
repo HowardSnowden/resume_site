4f46f16cd0564f4f80f7495f5f210a683d8c20f0
Damien Pyles <damien.pyles@readymaderc.com>
2017-11-12 12:17:39 -0500

    only display years for employment time

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,11 +2,11 @@ module PagesHelper
   
   def from_to(obj)
 
-	"#{obj.from.try(:strftime, '%B, %Y') } - #{date_or_present(obj.to)}" 
+	"#{obj.from.try(:strftime, '%Y') } - #{date_or_present(obj.to)}" 
   end
 
   def date_or_present(date)
   	 return unless date.is_a?(Date)
-  	 date >= (Date.today - 3.months) ? 'Present' : date.strftime('%B, %Y')
+  	 date >= (Date.today - 3.months) ? 'Present' : date.strftime('%Y')
   end
 end
